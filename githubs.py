d292b1c365454417710eeee45dd56257ff95e29c
rk254305 <rk254305@gmail.com>
2024-08-05 20:52:04 +0530

    Update githubs.py

---
+++ githubs.py
@@ -0,0 +1,24 @@
+'''
+#These are workflow of the github
+STEP 1:
+First create a repository
+#create repo on git hub
+STEP 2:
+Clone repo from server 
+# git clone <link of newly created repo>
+STEP 3:
+Make changes / create programs
+# write code of program and make changes
+STEP 4:
+Add changes to the server 
+# git add . <for adding all directory> or use git add <filename> for adding file
+STEP 5:
+Commit changes so changes take place on server
+# git commit -m "messege or details what you have changed"
+STEP 6:
+Push data and changes to the server
+# git push origin main <for pushing data to the server>
+After creating a file in local server use 
+# git init to create repo or reinitiate an existing
+'''
+print("WORKFLOW EXPLAINED")
